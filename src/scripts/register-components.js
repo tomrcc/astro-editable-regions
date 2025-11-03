a17d292a3da7c33104f253ba2e47469dc51ec8ad
tomrcc <tom@cloudcannon.com>
2025-11-04 11:48:21 +1300

    Fix import path

--- src/scripts/register-components.js
+++ src/scripts/register-components.js
@@ -1,5 +1,5 @@
 import { registerAstroComponent } from "@cloudcannon/editable-regions/astro";
-import PostHero from "../components/blog/post-hero/post-hero.astro";
+import PostHero from "../components/blog/post-hero.astro";
 // TODO: Can I use an alias for an import here?
 
 // Register your components
